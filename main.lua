20802f9d4dc85f499405916b6baf1551709e1cd1
Luca Giovani <lucagiovanige@gmail.com>
2024-06-27 08:00:26 +0200

    Skybox implemented

--- main.lua
+++ main.lua
@@ -8,18 +8,23 @@ local GAME_SCREEN
 GAME_TITLE = 'Chillfish Paradise'
 love.window.setTitle(GAME_TITLE)
 love.graphics.setDefaultFilter("nearest", "nearest")
-window_width = 1280 -- screen size must be == odd number to have perfect pixels
+window_width = 640 -- screen size must be == odd number to have perfect pixels
 window_height = 720
 SIZE_MULTIPLIER = 1 -- 'zooms' the image in or out. Default 1, can be changed for pixel reasons
-canvas = love.graphics.newCanvas(1280, 720)
+canvas = love.graphics.newCanvas(640, 720)
 
 -- game images
 IMG = {
-    ['sky'] = love.graphics.newImage('graphics/background.png')
+    ['skybox'] = love.graphics.newImage('graphics/skybox.png')
 }
+
+skybox_width = IMG['skybox']:getWidth()
+skybox_corner = skybox_width - window_width
 
 -- variable for storing and clearing keys pressed each update
 keys_pressed = {}
+camera_position = 0
+camera_movement = 1
 
 function love.keypressed(key)
     -- all inputs with few system-related exceptions are handled inside Game States    
@@ -39,14 +44,28 @@ function love.load()
     )
 
     --g.game_state:init()
-
-    -- creating new quad
-    new_quad = love.graphics.newQuad(0, 0, 100, 500, window_width, window_height)
 end
 
 function love.update(dt)
     --Timer.update(dt)
     --g.game_state:update()
+    
+    -- Scrolling text <--- WORKING!!!
+    camera_position = camera_position + 0.2
+
+    -- Camera movement by texture translation
+    if love.mouse.isDown(1) then
+		camera_position = camera_position + camera_movement
+    elseif love.mouse.isDown(2) then
+        camera_position = camera_position - camera_movement
+	end
+
+    -- Checking if texture is wrapping correctly
+    if camera_position > 0 then
+        camera_position = camera_position - skybox_corner -- note: this hardcoded value is the texture's right angle - a block's (screen) width
+    elseif camera_position < -skybox_corner then
+        camera_position = camera_position + skybox_corner
+    end
 end
 
 function love.draw()
@@ -54,7 +73,7 @@ function love.draw()
     
     -- setting canvas to final and drawing on it
     love.graphics.clear(0.5, 0.5, 0.5)
-    love.graphics.draw(IMG['sky'], new_quad, 0, 0)
+    love.graphics.draw(IMG['skybox'], camera_position, 0)
     
     -- drawing in a loop all the elements to be drawn on screen, removing dead ones
     --for i, img in ipairs(render_group) do
